d5d9cfd624e46a9bb35bf4bcd586b816147d5411
niorad <mail@niorad.com>
2017-05-03 17:14:03 +0200

    Initial Commit

---
+++ SDL2 Starter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL2 Starter
+//
+//  Created by Antonio Radovcic on 03.05.17.
+//  Copyright © 2017 niorad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
